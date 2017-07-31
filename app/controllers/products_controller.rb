25ba8b97b189c76b6493c6303c9827d2b9fe9e71
Your Name <you@example.com>
2017-07-31 17:27:54 -0500

    reto 13 temporal

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,28 @@
 class ProductsController < ApplicationController
   def index
-      @products = Product.all()
+    #   @products = Product.all()
+    @products = CategoryProduct.joins(:product,:category).select('products.id','products.name as productos','categories.name as categorias')
   end
+
+  def new
+    @product = Product.new
+    @categorys = Category.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.categoryProducts.create()
+    # @product.save
+    redirect_to '/products'
+  end
+
+  protected
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
+
+# select products.id,products.name,categories.name 
+# from category_products
+# inner join products on products.id = category_products.product_id
+# inner join categories on categories.id = category_products.category_id
